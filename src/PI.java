796145a7c823692756c167e58674436a0bc7ec42
杨俊艺 <1209032479@qq.com>
2018-06-11 04:34:01 +0800

    蒙特卡洛求PI调整循环次数,具体无改动

--- src/PI.java
+++ src/PI.java
@@ -1,11 +1,11 @@
 
 public class PI {
-	static double MontePI(int n) {
+	static double MontePI(double n) {
 		double PI;
 		double x, y;
-		int i, sum;
+		double i, sum;
 		sum = 0;
-		for (i = 1; i < n; i++) {
+		for (i = 1; i <= n; i++) {
 			x = Math.random();
 			y = Math.random();
 			if (x * x + y * y <= 1) {
@@ -17,14 +17,17 @@ public class PI {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(MontePI(100));
-		System.out.println(MontePI(1000));
-		System.out.println(MontePI(10000));
-		System.out.println(MontePI(100000));
-		System.out.println(MontePI(1000000));
-		System.out.println(MontePI(10000000));
-		System.out.println(MontePI(10000000));
-		System.out.println(MontePI(100000000));
+		for (int i = 0; i < 10; i++) {
+			System.out.println(MontePI(100));// 百
+			System.out.println(MontePI(1000));// 千
+			System.out.println(MontePI(10000));// 万
+			System.out.println(MontePI(100000));// 十万
+			System.out.println(MontePI(1000000));// 百万
+			System.out.println(MontePI(10000000));// 千万
+			System.out.println(MontePI(100000000));// 亿
+			System.out.println(MontePI(1000000000));// 十亿
+			System.out.println("\n");
+		}
 	}
 }
 // 蒙特卡洛求PI,结果不够精确,可能吧
